704cab248b97c3bbae4033dbc5ac5864ad534c1d
jose <zecapistolas@gmail.com>
2017-04-26 23:03:46 +0100

    Added custom function to register the time document was open.

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,4 +2,15 @@
  * Custom JS
  */
 
+$(document).ready(function() {
+  start = new Date().getTime();
+
+  $("#_form").submit(function(e) {
+    end = new Date().getTime();
+    timeSpent = Math.round((end - start) / 1000);
+
+    var elem = document.getElementById('user_time');
+    elem.value = timeSpent;
+  });
+});
 

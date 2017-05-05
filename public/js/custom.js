4dcfdba8ccb2e4298aef62377890ca2ddd434513
jose <zecapistolas@gmail.com>
2017-05-05 16:05:51 +0100

    Register the time an user spends per question.

--- public/js/custom.js
+++ public/js/custom.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     end = new Date().getTime();
     timeSpent = Math.round((end - start) / 1000);
 
-    var elem = document.getElementById('user_time');
+    var elem = document.getElementById('time_to_answer');
     elem.value = timeSpent;
   });
 });
